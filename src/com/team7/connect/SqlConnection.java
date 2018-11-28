71ee446823d9a6e3882e7039e0716be015f80580
lostredakos <e.giannakosian@gmail.com>
2018-11-28 10:47:33 +0200

    sqlconnection improved - main adjusted

--- src/com/team7/connect/SqlConnection.java
+++ src/com/team7/connect/SqlConnection.java
@@ -12,165 +12,128 @@ import java.util.ArrayList;
  *
  */
 public class SqlConnection {
-        /**
-     *
+    /**
      * @throws SQLException
      */
-    public void connect()throws SQLException {
+    public void connect() {
 
-        /**Declare querries*/
-
-        String query1 = "select * from owner;";
-        String query2 = "select * from vehicle;";
-        String query3 = "select * from insurance;";
-
-        /**Establish connection with db*/
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        Connection con= null;
-        try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost/project?autoReconnect=true&useSSL=false","root","0123456789");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        /**Dhmiourgia statement*/
+        /**Arxikopoihsh statement,connection kai resultset
+         * */
+        Connection con = null;
         Statement st = null;
-        try {
-            st = con.createStatement();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        /**Dhmiourgia resultset*/
-        ArrayList<Owner> ownerList = new ArrayList<>();
-        ArrayList<Vehicle> vehiList = new ArrayList<>();
-        ArrayList<Insurance> insuList = new ArrayList<>();
         ResultSet rs1 = null;
         ResultSet rs2 = null;
         ResultSet rs3 = null;
 
-        /**Owner resultSet
-         *
-         * 1 Owner Id
-         * 2 Owner Name
-         *
-         *
+        /**Declare querries
          * */
+        String query1 = "select * from owner;";
+        String query2 = "select * from vehicle;";
+        String query3 = "select * from insurance;";
 
+        /**Dhmiourgia listwn gia kathe resultset
+         * */
+        ArrayList<Owner> ownerList = new ArrayList<>();
+        ArrayList<Vehicle> vehiList = new ArrayList<>();
+        ArrayList<Insurance> insuList = new ArrayList<>();
+
+        /**Establish connection with db
+         * */
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/project?autoReconnect=true&useSSL=false", "root", "0123456789");
+
+            /**Dhmiourgia statement*/
+
+            st = con.createStatement();
+
+            /**Owner resultSet
+             *
+             * 1 Owner Id
+             * 2 Owner Name
+             *
+             *
+             * */
             rs1 = st.executeQuery(query1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        while (rs1.next()) {
-            try {
+            while (rs1.next()) {
+
                 Owner ownr = new Owner();
                 ownr.setOwnerName(rs1.getString(2));
                 ownr.setOwnerID(rs1.getString(1));
                 ownerList.add(ownr);
-                //System.out.println(ownr.getOwnerName()+ownr.getOwnerID());
-                //System.out.println(rs1.getString(1)+","+rs1.getString(2));
-            } catch (SQLException e) {
-                e.printStackTrace(); }
-        }
-        // TODO: SingletonDataSave.setSingletonOwner(ownerList);
+            }
+            OwnerController ownContr = new OwnerController(ownerList);
+            for (int i = 0; i < ownerList.size(); i++) {
+                System.out.println(ownerList.get(i));
+            }
 
+            /**Vehicle resultSet
+             * columns
+             * 1:ID
+             * 2:OwnerID
+             * 3:InsurID
+             * 4:VehiclePlate
+             */
 
-        OwnerController ownContr = new OwnerController(ownerList);
-        //ownContr.setOwnerList(ownerList);
-
-
-        for (int i=0; i<ownerList.size();i++){
-            System.out.println( ownerList.get(i));
-        }
-        rs1.close();
-
-
-        /**Vehicle resultSet
-
-         *
-         * columns
-         * 1:ID
-         * 2:OwnerID
-         * 3:InsurID
-         * 4:VehiclePlate
-         *
-         */
-
-        try {
             rs2 = st.executeQuery(query2);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        while (rs2.next()) {
-            try {
+            while (rs2.next()) {
                 Vehicle vehi = new Vehicle();
                 vehi.setVehLicensePlate(rs2.getString(4));
                 vehi.setInsurID(rs2.getString(3));
                 vehi.setOwnerID(rs2.getString(2));
                 vehi.setVehID(rs2.getString(1));
                 vehiList.add(vehi);
-                //System.out.println(rs2.getString(1)+","+rs2.getString(2)+","+rs2.getString(3)+","+rs2.getString(4));
-
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
-        }
-        //TODO: SingletonDataSave.setOneVehicle(Vehicle)
-        //SingletonDataSave.setSingletonVehicle(vehiList);
-        VehicleController vehiContr = new VehicleController(vehiList);
-        //vehiContr.setVehicleList(vehiList);
-        for (int i=0; i<ownerList.size();i++){
-            System.out.println( vehiList.get(i));
-        }
-        rs2.close();
+            VehicleController vehiContr = new VehicleController(vehiList);
+            for (int i = 0; i < vehiList.size(); i++) {
+                System.out.println(vehiList.get(i));
+            }
 
-        /**Insurance resultSet
-         *
-         * 1. Insu Id
-         * 2. Insu day From
-         * 3. Insu day To
-         *
-         * */
+            /**Insurance resultSet
+             * 1. Insu Id
+             * 2. Insu day From
+             * 3. Insu day To
+             * */
 
-        try {
             rs3 = st.executeQuery(query3);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        while (rs3.next()) {
-            try {
+            while (rs3.next()) {
                 Insurance insu = new Insurance();
                 insu.setInsuranceID(rs3.getString(1));
                 insu.setInsuranceFrom(rs3.getDate(2));
                 insu.setInsuranceTo(rs3.getDate(3));
                 insuList.add(insu);
-                System.out.println(rs3.getString(1)+","+rs3.getDate(2)+","+rs3.getDate(3));
 
-            } catch (SQLException e) {
-                e.printStackTrace();
+            }
+            InsuranceController insuContr = new InsuranceController(insuList);
+            for (int i = 0; i < insuList.size(); i++) {
+                System.out.println(insuList.get(i));
+            }
+
+            /**
+             * close connection with db
+             *
+             * */
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs1!= null)
+                    rs1.close();
+                if (rs2 != null)
+                    rs2.close();
+                if (rs3 != null)
+                    rs3.close();
+                if (st != null)
+                    st.close();
+                if (con != null)
+                    con.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
             }
         }
-        //TODO: SingletonDataSave.setOneInsurance(Insurance)
-        //SingletonDataSave.setSingletonInsurance(insuList);
-        InsuranceController insuContr = new InsuranceController(insuList);
-        //insuContr.setInsuranceList(insuList);
-        for (int i=0; i<ownerList.size();i++){
-            System.out.println( insuList.get(i));
-        }
-        rs3.close();
 
-        /**
-         * close connection with db
-         *
-         * */
-
-        st.close();
-        con.close();
     }
-
 }
