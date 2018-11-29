e2de32bb66c3950e3cd813c1e253c1f14e82b17e
Nikolaos Papazian <nikolaspapazian@gmail.com>
2018-11-29 17:37:26 +0200

    owner ticket set 0 euro

--- src/com/team7/Models/Owner.java
+++ src/com/team7/Models/Owner.java
@@ -12,7 +12,7 @@ package com.team7.Models;
 public class Owner implements Comparable {
     private String ownerID;
     private String ownerName;
-    private float TicketValue ;
+    private float TicketValue =Float.parseFloat("0,0");
 
 
     public String getOwnerID() {
